e183ddc99e025764374cda62c14cc7373d3352a7
Lucynn <olie8@wp.pl>
2021-02-11 17:44:09 +0000

    Navbar update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // Navigation
+
+$('.feat-btn').click(function(){
+    $('nav ul .feat-show').toggleClass("show");
+    $('nav ul .first').toggleClass("rotate");
+});
+
+$('nav ul li').click(function (){
+    $(this).addClass("active").siblings().removeClass("active");
+});
